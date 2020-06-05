fa39ab96ab17d9a31350ab7b36d0e39a2eac57c7
HafizSameerAli <hafizsameerali@gmail.com>
2020-06-06 01:05:01 +0500

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+
+Hello World
+
+    </div>
+  );
+}
+
+export default App;
