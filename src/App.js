c453e7d04f8975b570b5e47ed7419bad3b7878b3
HafizSameerAli <hafizsameerali@gmail.com>
2020-06-06 04:10:47 +0500

    update json

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div>
-Hellooo
+    <div className="App">
+      <h1>Hello World from all of us!</h1>      
     </div>
   );
 }
